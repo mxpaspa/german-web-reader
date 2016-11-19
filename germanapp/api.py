61dc588e5e938a6891cff8ee7441e7486d784261
mxpaspa <mxpaspa@gmail.com>
2016-11-19 17:46:48 +0000

    commit after saturday session

--- germanapp/api.py
+++ germanapp/api.py
@@ -5,10 +5,13 @@ from .database import session, Article
 from flask import flash
 
 
-
-@app.route("/api/display_article/{id}")
-def display_content():
-    article = session.query.filter_by(id = id)
+@app.route("/")
+def front_page():
+    return render_template("german.html")
+    
+@app.route("/api/display_article/<id>")
+def display_content(id):
+    article = session.query(Article).filter_by(id=id)
     return render_template("iframe.html", article = article)
 
 
